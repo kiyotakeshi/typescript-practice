fec089ddf0616868ab2405a9ca4a1c6e70f87a8d
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-03-14 08:54:36 +0900

    fix add function

--- app.ts
+++ app.ts
@@ -1,9 +1,18 @@
-function add(n1: number, n2: number) {
-    return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, phrase:  string) {
+
+    const result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    } else {
+        return result;
+    }
 }
 
-const number1 = 5;
+// 変数には型を指定していないのは、型推論(type inference)を利用しているから
+// 右辺が 5 なので number 型と推論される
+let number1 = 5;
 const number2 = 10;
+const printResult = true;
+const resultPhrase = 'Result: ';
 
-const result = add(number1, number2);
-console.log(result);
+add(number1, number2, printResult, resultPhrase);
