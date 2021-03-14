703f65261bf3e247b268891de1e5adce32edacc2
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-03-14 09:56:26 +0900

    tuple

--- app.ts
+++ app.ts
@@ -1,16 +1,30 @@
-// 明示的に型を書く場合
-// const person: {
-//     name: string;
-//     age: number;
-// }
-
-// 型推論が動作する場合は、型推論を使うほうがいい
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    // tuple 型の定義
+    role: [number, string];
+} = {
     name: 'mike',
     age: 30,
     // 文字列の array
     hobbies: ['Sports', 'Cooking'],
+
+    // tuple 型が配列と違う点は、要素数と要素の型が指定できること
+    role: [2, 'author'],
 };
+
+// // 型推論の場合
+// // role: (string | number) のユニオン型
+// // 3つ目の要素を入れられてしまう
+// person.role.push('admin');
+// // string か number であれば入れられてしまう
+// person.role[1] = 10;
+
+// tuple 型を明示した場合
+// person.role[1] = 10; // 型が違うから怒られる
+// person.role = []; // 長さの違う配列を代入するため怒られる
+// person.role.push('admin'); // 追加することはできてしまうので注意
 
 // let favariteActivities: string[];
 // favariteActivities = ['Sports', 'Cooking'];
