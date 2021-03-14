846c2412a3370a24be7847d9d79038982ccb4038
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-03-14 10:38:13 +0900

    literal

--- app.ts
+++ app.ts
@@ -1,18 +1,29 @@
 // union 型を使用
-function combine(input1: number | string, input2: number | string) {
+function combine(
+    input1: number | string,
+    input2: number | string,
+    // literal 型で引数の型を指定
+    resultConversion: 'as-number' | 'as-text'
+) {
     let result;
 
     // ランタイムでチェックする必要があるので処理を記載
-    if (typeof input1 == 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (
+        (typeof input1 == 'number' && typeof input2 === 'number') ||
+        resultConversion === 'as-number'
+    ) {
+        result = +input1 + +input2;
     } else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
 
-const combineAges = combine(30, 26);
+const combineAges = combine(30, 26, 'as-number');
 console.log(combineAges);
 
-const combinedNames = combine('Max', 'Anna');
+const combineStringAges = combine('30', '26', 'as-number');
+console.log(combineStringAges);
+
+const combinedNames = combine('Max', 'Anna', 'as-text');
 console.log(combinedNames);
